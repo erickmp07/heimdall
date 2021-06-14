c6ac4ddc3ca2e50083272ae7e2836008f503a2eb
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-06-14 19:17:41 -0300

    Uncomments the GPU verification

--- video.py
+++ video.py
@@ -19,9 +19,9 @@ import sys
 import os
 import os.path
 
-#physical_devices = tf.config.experimental.list_physical_devices('GPU')
-#assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-#tf.config.experimental.set_memory_growth(physical_devices[0], True)
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 model_size = (416, 416, 3)
 number_of_classes = 80
