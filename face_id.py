a9eaeccc7df924532af051a91e4a947eae08722d
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-05 00:59:47 -0300

    Print LFW dataset files extraction information

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cf5a261f25536b87d4fdc91abbd2e170857d2b97078db1731ac128e2b1a12fe
-size 2572
+oid sha256:20c1b48f850cbb5800ab982c5aed53e913f3a1bc8b8d76eec309a2cd6ff7b86c
+size 2780
