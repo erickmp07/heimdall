908a07ed4443f871f3e21714144a1f4489e06fbf
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-05 00:51:18 -0300

    Print directories making information

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e1aa83f40375174b21d40894b7773e49db0754b7a2180ea8ad507d491b17f9b
-size 2282
+oid sha256:7cf5a261f25536b87d4fdc91abbd2e170857d2b97078db1731ac128e2b1a12fe
+size 2572
