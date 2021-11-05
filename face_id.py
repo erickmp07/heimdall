fd2c538b60a53fa9fcb74f5f6119730d5391d6a1
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-05 01:16:06 -0300

    Log moving files information

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20c1b48f850cbb5800ab982c5aed53e913f3a1bc8b8d76eec309a2cd6ff7b86c
-size 2780
+oid sha256:0018ec33ac6a24ca3a9ee31b9f5bb1e224dfb4bd05ed22f013072efd525a9189
+size 3026
