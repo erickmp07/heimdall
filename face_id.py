030f4e9d50280df276dd6473a3320ee79b2a0318
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-05 00:22:01 -0300

    Make the directories

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb19ea1c0e6ae1eae1e38f664d2c1214bfd10d3e260b3b402fac2df7f03acb5c
-size 684
+oid sha256:1b537b6768af087b0100d109ec40bd35e64adacff5f30763620e9881d2295760
+size 969
