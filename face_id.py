8ef4f6a85fe4d5862fc2062829a1800ac56f3264
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-09 04:06:39 -0300

    Make a siamese model

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ac6355dfc1e8c68c949e2aa3324a5d05019cf2de7d39690403a3f317679c7a6
-size 6952
+oid sha256:5dce8b78d289265f3bf680483f5471ff2477798de067b0d38ac7f70b66e724c3
+size 7656
