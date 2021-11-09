2c04b8e732156c84a1c1564664f5ffbdf790925d
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-09 02:32:08 -0300

    Scale and resize an image to be between 0 and 1

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b25e98b92d0804c4fdd62a310f4117eef41b7f24adcec574eed466842cee248
-size 3867
+oid sha256:3bf13bfd5205528cf617d6b64a4497ecaf98a7a39774d529b6e0c570ed89cf29
+size 4301
