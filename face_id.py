c6bead4357d651b30a19765cc3b3ff2fa7209b39
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-09 03:07:34 -0300

    Build dataloader pipeline

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ba34521a304e06f4c72082057b151070e76f9b6b875bfc0c17f33f9596c409a
-size 4769
+oid sha256:a446de23220eb8c12de81dd7f60439bb15a5865c40fa41996611fa07fbc5b5db
+size 5517
