31b7828d6a160ac8b41c452546b6a5b67f8f24d1
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-09 03:42:07 -0300

    Build embedding layer

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2c34961edaffcb2549902a7a80cec83363735435a1ed5a4fb5a54cdd5902355
-size 5884
+oid sha256:34704e9a4950854d7a63c9e5fed10a22d3ac35b77138326bde1ae0367fef002c
+size 6607
