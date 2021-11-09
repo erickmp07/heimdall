efce9fca2bc8485b7f96988a47d65302ba885d52
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-09 01:57:55 -0300

    Fix methods doc

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b47075cf44c1ba26ffcb3acf1c5a90f8aaf9154528f47bfc4afef29a0759514
-size 3582
+oid sha256:32952b093ebbde4e9030e4df0d308635ea1c7454861e0e71f3a312335e4c8c55
+size 3642
