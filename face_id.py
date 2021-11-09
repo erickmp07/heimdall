5a9ed348d12dc92b25b57cc00f6faed0efe81236
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-09 03:17:06 -0300

    Build the testing partition

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:623179b4adfb55ea5539d9fb35a207b387f5180750d9763f34396855c77fdb88
-size 5734
+oid sha256:d2c34961edaffcb2549902a7a80cec83363735435a1ed5a4fb5a54cdd5902355
+size 5884
