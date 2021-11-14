3e122f8bf511e30f967d0d52263821ab7102cab9
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-14 20:24:41 -0300

    Build the step function

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d66841b6e2a737dd0658fa176a84b2522be1c1688f3cf5e4c1e76d33ccc4014
-size 9172
+oid sha256:d06d98e9c91e3de27c7bf42150387e7013d2627c78db76c7e88995cebe5adf53
+size 9835
