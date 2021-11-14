8ff89f96387687beefc98a634872829fa9681c97
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-14 20:04:01 -0300

    Setup Loss and Optimizer

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54a332fd375b839207644e9407813a9b25c1017bd47568ec593cee8fc605c031
-size 8868
+oid sha256:ac2e32096bc84538476505ca6d6d736f6a32930b22d2bbce39ceefe5f3423c80
+size 8971
