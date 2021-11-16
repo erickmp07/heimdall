3c1a0795bc3172dc44b3b022692b75e9f9ecaa49
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-16 01:26:33 -0300

    Fix L1Dist definition

--- face_id.py
+++ face_id.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17eca2ce7bd8189dd5d8f465b0b0682234be0782b5e423bfe5e7ae4837f422bd
-size 10973
+oid sha256:d1c89b00f8eb5b1607fdca4746971b4dbbeb2159ad2ac0adc9ddb4e3aff15b21
+size 10974
