8fb93f83b2933320389538a0179f0bc4ddc167f2
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-24 19:15:46 -0300

    Define the main layout components

---
+++ app/application.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.logger import Logger
+
+import cv2
+import tensorflow as tf
+from layers import L1Dist
+import os
+import numpy as np
+
+# Build app and layout
+class HeimdallApp(App):
+
+    def build(self):
+        # Main layout components
+        self.image1 = Image(size_hint=(1, 0.8))
+        self.button = Button(text='Verify', size_hint=(1, 0.1))
+        self.verification = Label(text="Verification Uninitiated", size_hint=(1, 0.1))
+
+
+if __name__ == '__main__':
+    HeimdallApp().run()
